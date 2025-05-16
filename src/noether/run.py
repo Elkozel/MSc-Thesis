a9ad5acca78da2f1a26e57605524b10865a0ec3b
Tsvetomir Hristov <caci802@gmail.com>
2025-05-17 01:19:40 +0200

    changing the run parameters

--- src/noether/run.py
+++ src/noether/run.py
@@ -1,12 +1,12 @@
-import comet_ml
 import torch
 from torch_geometric.utils import degree
 from pytorch_lightning.loggers import CometLogger
 from torch_geometric.data import Data
-from torch_geometric.transforms import RemoveDuplicatedEdges, RemoveSelfLoops, OneHotDegree, one_hot_degree
+from transforms import RemoveSelfLoops, RemoveDuplicatedEdges
+from lightning.pytorch.callbacks.early_stopping import EarlyStopping
 
 import lightning as L
-from models.Try2 import LitFullModel
+from models.Try1 import LitFullModel
 from utils.window_dataloader import MovingWindowDataloader
 from elasticsearch import Elasticsearch
 from utils.elastic_datafetcher import LANLGraphFetcher
@@ -46,21 +46,21 @@ default_hyper_params = {
     "test_to": 2_000_000,
     "window_size": 20, # How many past data's should be given to the RNN
 }
-# default_hyper_params = {
-#     "in_channels": 3,
-#     "hidden_channels": 15,
-#     "learning_rate": 0.001,
-#     "num_epochs": 1,
+default_hyper_params = {
+    "in_channels": 3,
+    "hidden_channels": 15,
+    "learning_rate": 0.001,
+    "num_epochs": 1,
 
-#     "seconds_bin": 60, # How many seconds of data to be put together into a single graph
-#     "train_from": 0,
-#     "train_to": 5_000,
-#     "valid_from": 5_001,
-#     "valid_to": 6_000,
-#     "test_from": 1_847_858,
-#     "test_to": 1_852_419,
-#     "window_size": 20, # How many past data's should be given to the RNN
-# }
+    "seconds_bin": 60, # How many seconds of data to be put together into a single graph
+    "train_from": 0,
+    "train_to": 5_000,
+    "valid_from": 2_301_630,
+    "valid_to": 2_302_630,
+    "test_from": 1_847_858,
+    "test_to": 1_852_419,
+    "window_size": 20, # How many past data's should be given to the RNN
+}
 comet_logger.log_hyperparams(default_hyper_params)
 
 es_url="http://localhost:9200"
@@ -68,9 +68,9 @@ hyper_params=default_hyper_params
 
 es = Elasticsearch(es_url)
 def transform(data: Data) -> Data:
-    rem_dub = RemoveDuplicatedEdges()
+    rem_dub = RemoveDuplicatedEdges(key=["edge_attr", "edge_weight", "time", "y"])
     data = rem_dub(data)
-    rem_self_loop = RemoveSelfLoops()
+    rem_self_loop = RemoveSelfLoops(attr=["edge_attr", "edge_weight", "time", "y"])
     data = rem_self_loop(data)
     
     in_degree = degree(data.edge_index[0], data.num_nodes)
@@ -95,9 +95,9 @@ test_data = LANLGraphFetcher(es, ("lanl-auth", "lanl-redteam"),
 
 model = LitFullModel(hyper_params["in_channels"], hyper_params["hidden_channels"])
 
+early_stop_callback = EarlyStopping(monitor="val_loss", mode="max")
     
-    
-trainer = L.Trainer(max_epochs=hyper_params["num_epochs"], logger=comet_logger)
+trainer = L.Trainer(max_epochs=hyper_params["num_epochs"], logger=comet_logger, callbacks=[early_stop_callback])
 trainer.fit(model=model, train_dataloaders=MovingWindowDataloader(train_data, hyper_params["window_size"]),
               val_dataloaders=MovingWindowDataloader(valid_data, hyper_params["window_size"]))
 trainer.test(model=model, dataloaders=MovingWindowDataloader(test_data, hyper_params["window_size"]))
