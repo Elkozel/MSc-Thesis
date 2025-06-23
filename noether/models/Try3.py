e0ff3594f286c111010e097c4c32b6cf1df0030c
Tsvetomir Hristov <caci802@gmail.com>
2025-06-23 18:27:35 +0200

    Small changes

--- noether/models/Try3.py
+++ noether/models/Try3.py
@@ -4,50 +4,43 @@ import torchmetrics
 import torch.nn as nn
 import lightning as L
 import torch.nn.functional as F
-from typing import Literal, Union
-from torch_geometric.nn import HEATConv
+from typing import List, Literal, Union
+from torch_geometric.nn import GATv2Conv
 from torch_geometric.utils import negative_sampling
-from torch_geometric.data import HeteroData, Batch
+from torch_geometric.data import Data, Batch
+from torch_geometric.data.data import BaseData
 import torchmetrics.classification
+from transformers import T5ForConditionalGeneration, AutoTokenizer
+
 
 
 class GNNEncoder(nn.Module):
-    def __init__(self, in_channels, hidden_channels, num_node_types, num_edge_types, edge_type_emb_dim, edge_dim, edge_attr_emb_dim, dropout_rate):
+    def __init__(self, in_channels, hidden_channels, dropout_rate, edge_dim):
         super().__init__()
-        self.conv1 = HEATConv(in_channels, hidden_channels*2, num_node_types, num_edge_types, edge_type_emb_dim, edge_dim, edge_attr_emb_dim, dropout=dropout_rate)
-        self.norm1 = nn.LayerNorm(hidden_channels*2)
-        self.conv2 = HEATConv(hidden_channels*2, hidden_channels*4, num_node_types, num_edge_types, edge_type_emb_dim, edge_dim, edge_attr_emb_dim, dropout=dropout_rate)
-        self.norm2 = nn.LayerNorm(hidden_channels*4)
-        self.conv3 = HEATConv(hidden_channels*4, hidden_channels*3, num_node_types, num_edge_types, edge_type_emb_dim, edge_dim, edge_attr_emb_dim, dropout=dropout_rate)
-        self.norm3 = nn.LayerNorm(hidden_channels*3)
-        self.conv4 = HEATConv(hidden_channels*3, hidden_channels*2, num_node_types, num_edge_types, edge_type_emb_dim, edge_dim, edge_attr_emb_dim, dropout=dropout_rate)
-        self.norm4 = nn.LayerNorm(hidden_channels*2)
-        self.conv5 = HEATConv(hidden_channels*2, hidden_channels, num_node_types, num_edge_types, edge_type_emb_dim, edge_dim, edge_attr_emb_dim, dropout=dropout_rate)
-        self.norm5 = nn.LayerNorm(hidden_channels)
+        # LLM for history encoding
+        self.llm = T5ForConditionalGeneration.from_pretrained('google/byt5-small')
+        self.llm_tokenizer = AutoTokenizer.from_pretrained('google/byt5-small')
+
+        self.conv1 = GATv2Conv(in_channels, hidden_channels*3, edge_dim=edge_dim)
+        self.norm1 = nn.LayerNorm(hidden_channels*3)
+        self.conv2 = GATv2Conv(hidden_channels*3, hidden_channels*2, edge_dim=edge_dim)
+        self.norm2 = nn.LayerNorm(hidden_channels*2)
+        self.conv3 = GATv2Conv(hidden_channels*2, hidden_channels, edge_dim=edge_dim)
+        self.norm3 = nn.LayerNorm(hidden_channels)
         
         self.dropout = dropout_rate
 
-    def forward(self, x, edge_index, node_type, edge_type, edge_features):
-
-        # x: Tensor, edge_index: Union[Tensor, SparseTensor], node_type: Tensor, edge_type: Tensor, edge_attr: Optional[Tensor] = None
-        out = self.conv1(x, edge_index, node_type, edge_type, edge_features)
+    def forward(self, x, edge_index, edge_features):
+        out = self.conv1(x, edge_index, edge_attr=edge_features)
         out = self.norm1(out)
         out = F.relu(out)
-        # out = F.dropout(out, p=self.dropout, training=self.training)
-        out = self.conv2(out, edge_index, node_type, edge_type, edge_features)
+        out = F.dropout(out, p=self.dropout, training=self.training)
+        out = self.conv2(out, edge_index, edge_attr=edge_features)
         out = self.norm2(out)
         out = F.relu(out)
-        # out = F.dropout(out, p=self.dropout, training=self.training)
-        out = self.conv3(out, edge_index, node_type, edge_type, edge_features)
+        out = F.dropout(out, p=self.dropout, training=self.training)
+        out = self.conv3(out, edge_index, edge_attr=edge_features)
         out = self.norm3(out)
-        out = F.relu(out)
-        # out = F.dropout(out, p=self.dropout, training=self.training)
-        out = self.conv4(out, edge_index, node_type, edge_type, edge_features)
-        out = self.norm4(out)
-        out = F.relu(out)
-        # out = F.dropout(out, p=self.dropout, training=self.training)
-        out = self.conv5(out, edge_index, node_type, edge_type, edge_features)
-        out = self.norm5(out)
         return out  # node embeddings
     
 class RNNEncoder(nn.Module):
@@ -103,10 +96,6 @@ class LitFullModel(L.LightningModule):
     def __init__(
         self,
         in_channels,
-        num_node_types,
-        num_edge_types,
-        edge_type_emb_dim,
-        edge_attr_emb_dim,
         hidden_channels = None,
         dropout_rate = 0.0,
         edge_dim = None,
@@ -116,20 +105,22 @@ class LitFullModel(L.LightningModule):
         binary_threshold = 0.5,
         negative_edge_sampling_min = 20,
         pred_alpha = 0.8,
-        model_name="Try2H"
+        model_name="Try2"
     ):
         super().__init__()
 
         if hidden_channels is None:
             hidden_channels = in_channels * 3
 
-        self.gnn = GNNEncoder(in_channels, hidden_channels, num_node_types, num_edge_types, edge_type_emb_dim, edge_dim, edge_attr_emb_dim, dropout_rate)
+        self.gnn = GNNEncoder(in_channels, hidden_channels, dropout_rate, edge_dim)
         self.rnn = RNNEncoder(hidden_channels, rnn_num_layers)
         self.link_pred = LinkPredictor(hidden_channels, 1)
         self.link_classifier = LinkTypeClassifier(hidden_channels, out_classes)
 
         self.link_predict_acc = torchmetrics.classification.BinaryAccuracy(threshold=binary_threshold)
         self.link_class_acc = torchmetrics.Accuracy(task="multiclass", num_classes=out_classes)
+        self.mal_count = torchmetrics.MeanMetric()
+        self.model_loss = torchmetrics.MeanMetric()
 
         self.rnn_window_size = rnn_window_size
         self.model_name = model_name
@@ -140,13 +131,13 @@ class LitFullModel(L.LightningModule):
         self.save_hyperparameters()
 
 
-    def forward(self, graph_sequence: Union[list[HeteroData], torch.Tensor], edge_pairs):
+    def forward(self, graph_sequence: Union[list[Data], torch.Tensor], edge_pairs):
         # Generate the features at each timestamp if not already computed
         # shape (timestamp, nodes, features)
         if isinstance(graph_sequence, torch.Tensor):
             graph_features = graph_sequence
         else:
-            graph_features = [self.gnn(*self.transform_hetero_data(data)) for data in graph_sequence]
+            graph_features = [self.gnn(data.x, data.edge_index, data.edge_attr) for data in graph_sequence]
             graph_features = torch.stack(graph_features)
 
         # Switch the order, so that each node is treated as a batch
@@ -166,96 +157,21 @@ class LitFullModel(L.LightningModule):
 
         return link_pred_scores, link_class_logits
     
-    def transform_hetero_data(self, data: HeteroData):
-        # ========================
-        #         Nodes          
-        # ========================
+    def precompute_features(self, graph_list: List[BaseData]) -> torch.Tensor:
+        features = [self.gnn(data.x, data.edge_index, data.edge_attr) for data in graph_list]
+        features = torch.stack(features)
+        return features
         
-        # Extract node features from the input data dictionary
-        x_dict = data.x_dict
-        
-        # Determine the maximum number of features (columns) across all node types
-        max_node_cols = max([t.size(1) for t in x_dict.values()])
-        
-        # Initialize lists to store node features and node types
-        x = []
-        node_type = []
-
-        # Loop through each node type and pad the features to match the max_node_cols
-        for type, t in enumerate(x_dict.values()):
-            # Calculate padding width required for each node type to match max_node_cols
-            pad_width = max_node_cols - t.size(1)
-            
-            # Pad node features with zeros (constant padding) to ensure all have the same column size
-            t = F.pad(t, (0, pad_width), mode='constant', value=0.0)
-            
-            # Append padded node features to the x list
-            x.append(t)
-            
-            # Create a tensor to denote the node type for each node in the current batch
-            node_type.append(torch.full((t.size(0),), type))
-
-        # Concatenate all node features into a single tensor
-        node_type = torch.cat(node_type)
-        x = torch.cat(x)
-
-        # ========================
-        #     Edge Attributes     
-        # ========================
-        
-        # Extract edge attributes from the input data dictionary
-        edge_attr_dict = data.edge_attr_dict
-        
-        # Determine the maximum number of edge features (columns) across all edge types
-        max_edge_cols = max([t.size(1) for t in edge_attr_dict.values()])
-
-        # Initialize lists to store edge attributes and edge types
-        edge_attr = []
-        edge_type = []
-
-        # Loop through each edge type and pad the features to match the max_edge_cols
-        for type, t in enumerate(edge_attr_dict.values()):
-            # Calculate padding width required for each edge type to match max_edge_cols
-            pad_width = max_edge_cols - t.size(1)
-            
-            # Pad edge features with zeros (constant padding) to ensure all have the same column size
-            t = F.pad(t, (0, pad_width), mode='constant', value=0.0)
-            
-            # Append padded edge features to the edge_attr list
-            edge_attr.append(t)
-            
-            # Create a tensor to denote the edge type for each edge in the current batch
-            edge_type.append(torch.full((t.size(0),), type))
-
-        # Concatenate all edge attributes into a single tensor
-        edge_attr = torch.cat(edge_attr)
-        edge_type = torch.cat(edge_type)
-
-        # ========================
-        #         Edges          
-        # ========================
-        
-        # Extract edge indices from the input data dictionary
-        edge_dict = data.edge_index_dict
-        
-        # Concatenate all edge indices (from different edge types) into a single tensor
-        edge_index = torch.cat(list(edge_dict.values()), dim=1)
-        
-        # Return the final processed data: node features, edge indices, node types, edge types, and edge attributes
-        return x, edge_index, node_type, edge_type, edge_attr
-
     
     def run_trough_batch(self, batch: Batch, num_windows: int, step: Literal['train', 'validation', 'test']):
+        graph_list = batch.to_data_list()        
         total_loss = torch.tensor(0.0).to(self.device)
-        total_pred_acc = torch.tensor(0.0).to(self.device)
-        total_class_acc = torch.tensor(0.0).to(self.device)
-        features = [self.gnn(*self.transform_hetero_data(data)) for data in batch.to_data_list()] # type: ignore
-        features = torch.stack(features)
+        gnn_features = self.precompute_features(graph_list)
             
         for i in range(num_windows):
             to_idx = i + self.rnn_window_size
-            x_features = features[i:to_idx]
-            _, edge_index, _, _, _ = self.transform_hetero_data(batch.get_example(to_idx)) # type: ignore
+            x_features = gnn_features[i:to_idx]
+            y_graph = batch.get_example(to_idx)
             
             assert y_graph.edge_index is not None
             assert isinstance(y_graph.y, torch.Tensor)
@@ -286,26 +202,25 @@ class LitFullModel(L.LightningModule):
             if positive_edges.any():
                 class_loss = F.cross_entropy(link_class, edge_labels.long())
                 class_acc = self.link_class_acc(link_class, edge_labels.int())
+                self.mal_count.update(edge_labels.count_nonzero() / edge_labels.size(0))
             else:
                 class_loss = torch.tensor(0.0, device=self.device)
-                class_acc = torch.tensor(0.0, device=self.device)
 
             loss = pred_loss + self.pred_alpha * class_loss
+
+            # Update metrics
+            self.model_loss.update(loss)
             pred_acc = self.link_predict_acc(link_pred, labels.int())
             
-            
             total_loss += loss
-            total_pred_acc += pred_acc
-            total_class_acc += class_acc
 
         avg_loss = total_loss / num_windows
-        avg_pred_acc = total_pred_acc / num_windows
-        avg_class_acc = total_class_acc / num_windows
 
         return {
             "avg_loss": avg_loss,
-            "avg_pred_acc": avg_pred_acc,
-            "avg_class_acc": avg_class_acc
+            "avg_pred_acc": self.link_predict_acc.compute(),
+            "avg_class_acc": self.link_class_acc.compute(),
+            "avg_mal_count": self.mal_count.compute()
         }
 
     def training_step(self, batch: Batch, batch_idx):
@@ -323,6 +238,7 @@ class LitFullModel(L.LightningModule):
         self.log("train_loss", results["avg_loss"], batch_size=batch.num_graphs, on_epoch=True)
         self.log("train_pred_acc", results["avg_pred_acc"], batch_size=batch.num_graphs, prog_bar=True, on_epoch=True)
         self.log("train_class_acc", results["avg_class_acc"], batch_size=batch.num_graphs, prog_bar=True, on_epoch=True)
+        self.log("train_mal_count", results["avg_mal_count"], batch_size=batch.num_graphs)
 
         return results["avg_loss"]
     
@@ -341,6 +257,7 @@ class LitFullModel(L.LightningModule):
         self.log("val_loss", results["avg_loss"], batch_size=batch.num_graphs, on_epoch=True)
         self.log("val_pred_acc", results["avg_pred_acc"], batch_size=batch.num_graphs, prog_bar=True, on_epoch=True)
         self.log("val_class_acc", results["avg_class_acc"], batch_size=batch.num_graphs, prog_bar=True, on_epoch=True)
+        self.log("val_mal_count", results["avg_mal_count"], batch_size=batch.num_graphs)
 
         return results["avg_loss"]
     
