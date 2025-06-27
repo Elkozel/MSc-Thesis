8be952b72548e398eb8a39a815d03db2c098d059
Tsvetomir Hristov <caci802@gmail.com>
2025-06-27 18:54:24 +0200

    adding arguments

--- noether/run.py
+++ noether/run.py
@@ -1,3 +1,4 @@
+import argparse
 import comet_ml
 from comet_ml.integration.pytorch import log_model
 import torch
@@ -13,11 +14,29 @@ import lightning as L
 from models.Try1 import LitFullModel as Try1
 from models.Try2 import LitFullModel as Try2
 from models.Try2H import LitFullModel as Try2H
+from models.Try3 import LitFullModel as Try3
 from datasets.UFW22_local import UFW22L
 from datasets.UFW22H_local import UFW22HL
 from datasets.LANL_local import LANLL
 
-if __name__ == '__main__':
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Train and test a GNN model with Comet logging")
+    
+    parser.add_argument('--model', type=str, choices=['try1', 'try2', 'try2h', 'try3'], required=True,
+                        help="Model type to use")
+    parser.add_argument('--dataset', type=str, choices=['UFW22', 'UFW22h', 'LANL'], required=True,
+                        help="Dataset to use")
+    parser.add_argument('--max-epochs', type=int, default=50,
+                        help="Maximum number of training epochs")
+    
+    return parser.parse_args()
+
+def check_hetero(model_name: str, dataset_name: str):
+    return model_name.endswith("h") ^ dataset_name.endswith("h")
+
+def main():
+    args = parse_args()
     # Initialize a Comet experiment for logging metrics
     comet_logger = CometLogger(
         api_key="CcXihX0g0UcdsprkmlKiD19Z8",
@@ -33,11 +52,11 @@ if __name__ == '__main__':
         # AddInOutDegree()
     ]
 
-    if dataset == "UFW22":
+    if args.dataset == "UFW22":
         dataset = UFW22L("/data/datasets/UWF22", transforms=transformations)
-    elif dataset == "UFW22H":
+    elif args.dataset == "UFW22h":
         dataset = UFW22HL("/data/datasets/UWF22", transforms=transformations)
-    elif dataset == "LANL":
+    elif args.dataset == "LANL":
         dataset = LANLL(
             "/data/datasets/LANL",
             download=True, 
@@ -46,21 +65,21 @@ if __name__ == '__main__':
     else:
         raise NotImplementedError(f"Dataset {dataset} is not implemented")
 
-    if model == "try1":
+    if args.model == "try1":
         model = Try1(
         dataset.node_features,
         dataset.node_features * 3,
         out_classes = dataset.num_classes,
         dropout_rate = 0.0
         )
-    elif model == "try2":
+    elif args.model == "try2":
         model = Try2(
         dataset.node_features,
         out_classes = dataset.num_classes,
         pred_alpha = 1.1,
         edge_dim = dataset.edge_features
         )
-    elif model == "try2h":
+    elif args.model == "try2h":
         model = Try2H(
         dataset.node_features,
         dataset.num_node_types, # type: ignore
@@ -70,6 +89,13 @@ if __name__ == '__main__':
         out_classes = dataset.num_classes,
         pred_alpha = 1.1,
         edge_dim = dataset.edge_features,
+        )
+    elif args.model == "try3":
+        model = Try3(
+        dataset.node_features,
+        out_classes = dataset.num_classes,
+        pred_alpha = 1.1,
+        edge_dim = dataset.edge_features
         )
     else:
         raise NotImplementedError(f"Model {model} is not implemented")
@@ -83,7 +109,7 @@ if __name__ == '__main__':
     # If we train on tensor cores as well
     torch.set_float32_matmul_precision('high')
 
-    trainer = L.Trainer(max_epochs = 50,
+    trainer = L.Trainer(max_epochs = args.max_epochs,
                         logger=comet_logger,  # type: ignore
                         callbacks=[EarlyStopping(monitor="val_loss", mode="min", check_on_train_epoch_end=False)]) # type: ignore
     trainer.fit(model, dataset)
